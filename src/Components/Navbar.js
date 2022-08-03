afae4e3147a096243b2dc8081c40d3bc6f3338d6
HI MESH <heem.ace@gmail.com>
2022-08-03 10:01:52 +0545

    updates

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,7 @@ import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import Logo from "../Images/logo.png";
 import { Link } from "react-scroll";
-import resume from "../Images/Himesh Bhandari.pdf";
+import resume from "../Images/HimeshCV.pdf";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -104,7 +104,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="https://github.com/HIMESH1998"
+              href="https://github.com/AETHER010"
               target="_blank"
               rel="noreferrer"
             >
@@ -114,7 +114,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#bd0fb7]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="/"
+              href="https://mail.google.com/mail/u/0/#inbox"
               target="_blank"
               rel="noreferrer"
             >
