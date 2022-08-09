58c1e58af67968af4f1f22d9d6963c4997a6e413
HI MESH <heem.ace@gmail.com>
2022-08-09 09:44:55 +0545

    changes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,7 @@ import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import Logo from "../Images/logo.png";
 import { Link } from "react-scroll";
-import resume from "../Images/HimeshCV.pdf";
+import resume from "../Images/Himesh BhandariCV.pdf";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
