18ce680fa3a9b9db787892918d57766acaa12600
HI MESH <heem.ace@gmail.com>
2022-08-22 10:22:40 +0545

    changes

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from "react";
-import note from "../Images/note.png";
+import Ecommerce from "../Images/Ecommerce.png";
+import Music from "../Images/Music.png";
 import fps from "../Images/fps.png";
 import car from "../Images/car.png";
 import TravelDairy from "../Images/TravelDairy.png";
-import Blog from "../Images/Blog.png";
 import Aos from "aos";
 import "aos/dist/aos.css";
 
@@ -30,7 +30,7 @@ const Work = () => {
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           <div
             data-aos="flip-left"
-            style={{ backgroundImage: `url(${note})` }}
+            style={{ backgroundImage: `url(${Music})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             <div className="opacity-0 group-hover:opacity-100">
@@ -93,13 +93,13 @@ const Work = () => {
 
           <div
             data-aos="flip-left"
-            style={{ backgroundImage: `url(${Blog})` }}
+            style={{ backgroundImage: `url(${Ecommerce})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                MERN Stack Application
+                React JS Application
               </span>
               <div className="pt-8 text-center">
                 <a href="https://github.com/HIMESH1998/MERN-Blog">
